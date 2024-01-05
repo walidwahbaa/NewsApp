b11674f354ee58082e5a1a20f608f34d7552eb1a
walidwahbaa <walidwahba5000@gmail.com>
2024-01-05 16:07:54 +0300

    adds the article text and image from the json file

---
+++ NewsApp/ArticleCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleCell.swift
+//  NewsApp
+//
+//  Created by walid wahba on 5.01.2024.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
